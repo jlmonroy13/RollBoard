3a83684e3c7f19554a32fb06fb2d32d618032ea2
Jorge Monroy <jlmonroy13@gmail.com>
2015-11-09 00:28:34 -0500

    gallery markup ready and update with Selvio

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 $('.home_features').parallax({imageSrc:'img/bckg/home_features_bckg.jpg'});
 
 
+$('.btn-minuse').on('click', function(){
+        $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) - 1)
+})
+
+$('.btn-pluss').on('click', function(){
+        $(this).parent().siblings('input').val(parseInt($(this).parent().siblings('input').val()) + 1)
+})
+
+
 
 
